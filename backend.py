173612bfecf8b81e0dc6cb6f092f6ccfedc95c34
lohith-gmail <lohith.srt@gmail.com>
2019-06-25 13:35:56 +0530

    included app routing

--- backend.py
+++ backend.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def indexPage():
+def index():
+    return render_template('home.html')
+
+
+
+@app.route('/nameMovie')
+def moviePageDisplay():
     return render_template('index.html')
+
+
 
 
 if __name__ == '__main__':
